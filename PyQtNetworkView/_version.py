afb70d1f21483c2c9e56f7709a9a73211000d84d
Nicolas ELIE <nicolas.elie@cnrs.fr>
2019-05-16 15:13:08 +0200

    Update _version

--- PyQtNetworkView/_version.py
+++ PyQtNetworkView/_version.py
@@ -1,9 +1,9 @@
-# THIS FILE IS GENERATED FROM LIBMETGEM SETUP.PY
+# THIS FILE IS GENERATED FROM PyQtNetworkView SETUP.PY
 
-short_version = '0.2.0'
-version = '0.2.0'
-full_version = '0.2.0'
-git_revision = 'cf834a3a22d34979eeb6b68610f85c664e1f9b91'
+short_version = '0.3.0'
+version = '0.3.0'
+full_version = '0.3.0'
+git_revision = '941c543af0a3a57a019429950ce5bee82591e4bb'
 release = True
 if not release:
     version = full_version
