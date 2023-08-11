058b0c11ac9896cf58e5134e31bd051913de046f
Nicolas ELIE <nicolas.elie@cnrs.fr>
2023-08-11 15:53:16 +0200

    Use pip --config-settings flag instead of deprecated --global-option

--- recipe/build-python.sh
+++ recipe/build-python.sh
@@ -1,1 +1,9 @@
-python -m pip install --global-option="build_ext" --global-option="-DPython3_EXECUTABLE:FILEPATH=${PYTHON}" . --no-build-isolation --no-deps --ignore-installed --no-index --no-cache-dir -vv
+python -m pip install . \
+    --config-settings="--global-option=build_ext" \
+    --config-settings="--global-option=-DPython3_EXECUTABLE:FILEPATH=${PYTHON}" \
+    --no-build-isolation \
+    --no-deps \
+    --ignore-installed \
+    --no-index \
+    --no-cache-dir \
+    -vv
